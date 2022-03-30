ec3a235b1bd518745a742354a949d0cdde7dbdab
Syed Furquan Ahmad <furquan@pm.me>
2022-03-30 22:49:44 +0530

    Added ComponentStyles module (no styles only colors at this point

---
+++ components/ComponentStyles.js
@@ -0,0 +1,15 @@
+const colors0 = {
+    blued: '#113f67',
+    bluel: '#38598b',
+    purplec: '#a2a8d3',
+    mist: '#e7eaf6'
+};
+
+const colors1 = {
+    blued: '#5585b5',
+    verdigris: '#53a8b6',
+    blueh: '#79c2d0',
+    icej: '#bbe4e9'
+}
+
+export { colors0, colors1 };
