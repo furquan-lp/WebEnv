8f2789f7059f03bc16ce4408adbba710defebfdb
Syed Furquan Ahmad <furquan@pm.me>
2022-03-30 23:48:39 +0530

    Added AboutModal in main App

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
+import { useState } from 'react';
 import { View, StatusBar } from 'react-native';
 import TopBar from './components/TopBar';
+import AboutModal from './components/AboutModal';
 import { colors0 } from './components/ComponentStyles';
 
 const App = () => {
+  const [visible, setModalVisible] = useState(false);
   return (
     <View>
       <StatusBar
         animated={true}
         backgroundColor={colors0.blued}
         hidden={false} />
-      <TopBar />
+      <TopBar aboutButton={() => setModalVisible(true)} />
+      <AboutModal visible={visible} setModalVisible={setModalVisible} />
     </View>
   );
 };
