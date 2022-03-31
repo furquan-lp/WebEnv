a291d56a457caa7911b605244857037cc818122d
Syed Furquan Ahmad <furquan@pm.me>
2022-03-31 22:59:05 +0530

    Added the data cards, the web wrapper

--- App.js
+++ App.js
@@ -2,18 +2,39 @@ import { useState } from 'react';
 import { View, StatusBar } from 'react-native';
 import TopBar from './components/TopBar';
 import AboutModal from './components/AboutModal';
-import { colors0 } from './components/ComponentStyles';
+import Cards from './components/Cards';
+import { colors0, colors1 } from './components/ComponentStyles';
+import { StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors1.verdigris,
+    alignItems: 'stretch',
+    //justifyContent: 'center',
+    display: 'block'
+  },
+  webWrapper: {
+    flex: 1,
+    height: 720,
+    width: 480,
+    alignSelf: 'center'
+  }
+});
 
 const App = () => {
   const [visible, setModalVisible] = useState(false);
   return (
-    <View>
-      <StatusBar
-        animated={true}
-        backgroundColor={colors0.blued}
-        hidden={false} />
-      <TopBar aboutButton={() => setModalVisible(true)} />
-      <AboutModal visible={visible} setModalVisible={setModalVisible} />
+    <View style={style.webWrapper}>
+      <View style={style.container}>
+        <StatusBar
+          animated={true}
+          backgroundColor={colors0.blued}
+          hidden={false} />
+        <TopBar aboutButton={() => setModalVisible(true)} />
+        <AboutModal visible={visible} setModalVisible={setModalVisible} />
+        <Cards temperature={33.4} humidity={61} />
+      </View>
     </View>
   );
 };
