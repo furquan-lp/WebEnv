7fbe0c1ce3d78ec5f706881ba711904ba7bfea41
Syed Furquan Ahmad <furquan@pm.me>
2022-04-12 00:25:30 +0530

    Changed color of the loading indicator to colors1.verdigris

--- components/CircularLoading.js
+++ components/CircularLoading.js
@@ -2,8 +2,8 @@ import { ActivityIndicator, StyleSheet, View } from "react-native";
 import { colors1 } from "./ComponentStyles";
 
 const CircularLoading = () =>
-  <View style={[styles.container]}>
-    <ActivityIndicator size="large" color={colors1.icej} />
+  <View style={styles.container}>
+    <ActivityIndicator size="large" color={colors1.verdigris} />
   </View>;
 
 const styles = StyleSheet.create({
