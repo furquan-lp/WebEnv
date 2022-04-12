3fd890ea2670091bf7eaf472b40e8e55c49e485c
Syed Furquan Ahmad <furquan@pm.me>
2022-04-13 00:27:38 +0530

    Replace URLBar Icon.Button with a Pressable Icon with the relevant styles

--- components/URLBar.js
+++ components/URLBar.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, TextInput } from "react-native";
+import { View, TextInput, Pressable } from "react-native";
 import { StyleSheet } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { colors0, colors1 } from "./ComponentStyles";
@@ -8,17 +8,31 @@ const styles = StyleSheet.create({
   URLArea: {
     flex: 1,
     flexDirection: 'row',
-    padding: 10
+    padding: 6,
+    height: 40,
+    marginVertical: 5
   },
   URLInput: {
-    flex: 1,
+    flex: 4,
+    height: 40,
     padding: 5,
-    borderRadius: 2,
+    borderRadius: 4,
     borderWidth: 1,
     borderColor: colors1.icej,
     backgroundColor: 'white',
-    fontSize: 22,
+    fontSize: 20,
     fontFamily: 'monospace'
+  },
+  button: {
+    flex: 1,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 4,
+    backgroundColor: colors1.verdigris
+  },
+  active: {
+    backgroundColor: colors0.purplec
   }
 });
 
@@ -31,13 +45,18 @@ const URLField = ({ URL, setURL }) => {
         style={styles.URLInput}
         onSubmitEditing={() => setURL(text)}
       />
-      <Icon.Button
-        name="check"
-        backgroundColor={colors1.verdigris}
-        borderRadius={4}
-        size={26}
-        onPress={() => setURL(text)}
-      />
+      <Pressable onPress={() => setURL(text)}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.active : null]}
+      >
+        <Icon
+          name="check"
+          color="white"
+          iconStyle={{ marginRight: 5, marginLeft: 5 }}
+          size={26}
+        />
+      </Pressable>
     </View>);
 };
 
