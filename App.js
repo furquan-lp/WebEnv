94eaf946d960f0a5b3c5c3a7d18c607085ef3def
Syed Furquan Ahmad <furquan@pm.me>
2022-04-13 17:35:15 +0530

    Use hardcoded ngrok URL

--- App.js
+++ App.js
@@ -50,7 +50,7 @@ const WebEnv = () => {
   const [chartType, setChartType] = useState(0);
   const [env, setEnv] = useState([]);
   const [chartData, setChartData] = useState({ labels: [-1], data: [0.0] });
-  const [URL, setURL] = useState('http://192.168.43.126/');
+  const [URL, setURL] = useState('http://4.tcp.ngrok.io:17126');
 
   useEffect(() => {
     setTimeout(() => {
