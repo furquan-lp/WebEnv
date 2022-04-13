9a3eed859b7201f6d1a8b1a5df58435b6f1a879a
Syed Furquan Ahmad <furquan@pm.me>
2022-04-13 16:04:09 +0530

    Added methods to fetch version, repository URL and Platform.OS

--- services/WEUtils.js
+++ services/WEUtils.js
@@ -1,7 +1,22 @@
 import { Platform } from 'react-native';
 
+const appVer = '0.8.5-beta';
+const repoURL = 'https://github.com/furquan-lp/WebEnv';
+
 const isWeb = () => {
-    return (Platform.OS === 'web');
+  return (Platform.OS === 'web');
+};
+
+const getOS = () => {
+  return Platform.OS;
+};
+
+const getAppVersion = () => {
+  return appVer;
+};
+
+const getRepoURL = () => {
+  return repoURL;
 }
 
-export default { isWeb };
+export default { isWeb, getOS, getAppVersion, getRepoURL };
