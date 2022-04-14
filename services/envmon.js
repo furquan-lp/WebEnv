dd5cef8a33ed0606e082b685a0d36af8b29f90ed
Syed Furquan Ahmad <furquan@pm.me>
2022-04-14 15:05:19 +0530

    Added additional parameter requestHeader to getAll() to allow for passing request headers to axios.get()

--- services/envmon.js
+++ services/envmon.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-const getAll = (backendURL) => {
-  return axios.get(backendURL).then(response => response.data);
+const getAll = (backendURL, requestHeader) => {
+  if (requestHeader === undefined)
+    return axios.get(backendURL).then(response => response.data);
+  else
+    return axios.get(backendURL, { headers: requestHeader }).then(response => response.data);
 }
 
 export default { getAll };
