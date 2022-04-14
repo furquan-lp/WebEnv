5b08c6c23600328360844c8ba830a2647504cdf6
Syed Furquan Ahmad <furquan@pm.me>
2022-04-14 15:09:32 +0530

    Added new init() function which fetches (and sets) the backend URL (and other information) from a remote JSON API, changed appVer and repoURL from const to let to allow the init function to modify them

--- services/WEUtils.js
+++ services/WEUtils.js
@@ -1,7 +1,21 @@
 import { Platform } from 'react-native';
+import envmon from './envmon';
 
-const appVer = '0.9.0-alpha.ngrok';
-const repoURL = 'https://github.com/furquan-lp/WebEnv';
+let appVer = '0.9.3-beta.ngrok';
+let repoURL = 'https://github.com/furquan-lp/WebEnv';
+
+const init = (setURL, URL) => {
+  envmon.getAll(
+    'https://api.jsonbin.io/v3/b/6257c74280883c3054e15f27/latest',
+    { 'X-Master-Key': '$2b$10$pWNGBg9x/gcFna2bGzV2DO.97lv6XCoK35tPfs4e.HlgJAdpZ8aC.' }
+  ).then(webenvData => {
+    appVer = webenvData.record.version;
+    repoURL = webenvData.repositoryURL;
+    const ngrokURL = webenvData.record.backendURL;
+    if (ngrokURL !== URL)
+      setURL(webenvData.record.backendURL);
+  });
+}
 
 const isWeb = () => {
   return (Platform.OS === 'web');
@@ -19,4 +33,4 @@ const getRepoURL = () => {
   return repoURL;
 }
 
-export default { isWeb, getOS, getAppVersion, getRepoURL };
+export default { init, isWeb, getOS, getAppVersion, getRepoURL };
